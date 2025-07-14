dfed6ccf7e0dd531ef91291cdb45847735adfbea
rafsun-riad <mruhaquer@gmail.com>
2025-07-14 11:39:13 +0600

    update: more filter option added

--- chat_server/serializers.py
+++ chat_server/serializers.py
@@ -1,10 +1,34 @@
 from rest_framework import serializers
 
-from .models import Server
+from .models import Channel, Server
+
+
+class ChannelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Channel
+        fields = "__all__"
+        read_only_fields = ("id", "created_at", "updated_at")
 
 
 class ServerSerializer(serializers.ModelSerializer):
+    num_members = serializers.SerializerMethodField()
+    channel_server = ChannelSerializer(many=True, read_only=True)
+
     class Meta:
         model = Server
-        fields = "__all__"
+        exclude = ("member",)
         read_only_fields = ("id", "created_at", "updated_at")
+
+    def get_num_members(self, obj):
+        return (
+            obj.num_members
+            if hasattr(obj, "member") and hasattr(obj, "num_members")
+            else None
+        )
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        num_members = self.context.get("num_members")
+        if not num_members:
+            representation.pop("num_members")
+        return representation
--- chat_server/views.py
+++ chat_server/views.py
@@ -1,4 +1,6 @@
+from django.db.models import Count
 from rest_framework import viewsets
+from rest_framework.exceptions import AuthenticationFailed, ValidationError
 from rest_framework.response import Response
 
 from .models import Server
@@ -14,15 +16,37 @@ class ServerListViewSet(viewsets.ViewSet):
         category = request.query_params.get("category", None)
         qty = request.query_params.get("qty", None)
         by_user = request.query_params.get("by_user") == "true"
+        by_serverid = request.query_params.get("by_serverid", None)
+        with_num_members = request.query_params.get("with_num_members") == "true"
+
+        if (by_user or by_serverid) and not request.user.is_authenticated:
+            raise AuthenticationFailed(
+                "You must be authenticated to access this resource."
+            )
 
         if category:
-            self.queryset = self.queryset.filter(category=category)
+            self.queryset = self.queryset.filter(category__name__icontains=category)
 
         if by_user:
             self.queryset = self.queryset.filter(member=request.user.id)
 
+        if by_serverid:
+            try:
+                self.queryset = self.queryset.filter(id=by_serverid)
+                if not self.queryset.exists():
+                    raise ValidationError(
+                        f"Server with this ID {by_serverid} does not exist."
+                    )
+            except ValueError:
+                raise ValidationError("Invalid server ID format.")
+
+        if with_num_members:
+            self.queryset = self.queryset.annotate(num_members=Count("member"))
+
         if qty:
             self.queryset = self.queryset[: int(qty)]
 
-        serializer = ServerSerializer(self.queryset, many=True)
+        serializer = ServerSerializer(
+            self.queryset, many=True, context={"num_members": with_num_members}
+        )
         return Response(serializer.data)

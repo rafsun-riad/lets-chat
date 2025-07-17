4e46cb86f63da38845f1e056b50658b9ecb2619b
rafsun-riad <mruhaquer@gmail.com>
2025-07-17 15:12:25 +0600

    update: making chat service

--- backend/routing.py
+++ backend/routing.py
@@ -2,9 +2,13 @@ from channels.routing import URLRouter
 from django.urls import path
 
 from chat_server.routing import websocket_urlpatterns as chat_websocket_urlpatterns
+from chat_service.routing import (
+    websocket_urlpatterns as chat_service_websocket_urlpatterns,
+)
 
 websocket_urlpatterns = [
     path(
-        "ws/", URLRouter(chat_websocket_urlpatterns)
+        "ws/",
+        URLRouter([chat_websocket_urlpatterns, chat_service_websocket_urlpatterns]),
     ),  # for multiple websocket connections URLRouter([chat_websocket_urlpatterns, ...])
 ]
